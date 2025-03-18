99fed78b6c375e74ccdb280a2f05bbe6b361ee06
Martin de Oro <deoromartinignacio@gmail.com>
2025-03-18 11:38:24 -0300

    changes

--- src/pages/post-page/PostPage.tsx
+++ src/pages/post-page/PostPage.tsx
@@ -1,87 +1,67 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { StyledContainer } from "../../components/common/Container";
 import Tweet from "../../components/tweet/Tweet";
 import Loader from "../../components/loader/Loader";
-import { HttpService } from "../../service/HttpRequestService";
+import { useHttpRequestService } from "../../service/HttpRequestService";
 import TweetBox from "../../components/tweet-box/TweetBox";
 import { StyledH5 } from "../../components/common/text";
 import { StyledFeedContainer } from "../home-page/components/contentContainer/FeedContainer";
 import CommentFeed from "../../components/feed/CommentFeed";
 
-interface PostPageState{
-    postId: string
-    post?: any 
-}
+import { useQuery } from "@tanstack/react-query";
+import { Post } from "../../service";
 
-class PostPage extends Component<{}, PostPageState> {
-  private service: any
 
-  constructor(props: {}) {
-    super(props);
 
-    this.state = {
-      postId: window.location.href.split("/")[4],
-      post: undefined,
-    };
+const PostPage = () => {
+  const [postId, setPostId] = useState(window.location.href.split("/")[4]);
+  const [post, setPost] = useState<Post>();
+  const service = useHttpRequestService();
 
-    this.service = new HttpService().service;
-  }
+  const postQuery = useQuery({
+    queryKey: ["post", postId],
+    queryFn: (): Promise<Post> => service.getPostById(postId)
+  })
 
-  componentDidMount() {
-    this.fetchPost();
-  }
-
-  componentDidUpdate(prevProps: {}, prevState: PostPageState) {
-    if (prevState.postId !== this.state.postId) {
-      this.fetchPost();
+  useEffect(() => {
+    if(postQuery.status === 'success'){
+      setPost(postQuery.data)
     }
-  }
+  }, [postQuery.status, postQuery.data, postId])
 
-  fetchPost() {
-    this.service
-      .getPostById(this.state.postId)
-      .then((res: any) => {
-        this.setState({ post: res });
-      })
-      .catch((e: any) => {
-        console.log(e);
-      });
-  }
 
-  render() {
-    return (
-      <StyledContainer borderRight={"1px solid #ebeef0"}>
-        <StyledContainer
-          padding={"16px"}
-          borderBottom={"1px solid #ebeef0"}
-          maxHeight={"53px"}
-        >
-          <StyledH5>Tweet</StyledH5>
-        </StyledContainer>
-        <StyledFeedContainer>
-          {this.state.post ? (
-            <>
-              <Tweet post={this.state.post} />
-              <StyledContainer
-                borderBottom={"1px solid #ebeef0"}
-                padding={"16px"}
-              >
-                <TweetBox parentId={this.state.postId} />
-              </StyledContainer>
-
-              <StyledContainer minHeight={"53.5vh"}>
-                <CommentFeed postId={this.state.postId} />
-              </StyledContainer>
-            </>
-          ) : (
-            <StyledContainer justifyContent={"center"} alignItems={"center"}>
-              <Loader />
-            </StyledContainer>
-          )}
-        </StyledFeedContainer>
+  return (
+    <StyledContainer borderRight={"1px solid #ebeef0"}>
+      <StyledContainer
+        padding={"16px"}
+        borderBottom={"1px solid #ebeef0"}
+        maxHeight={"53px"}
+      >
+        <StyledH5>Tweet</StyledH5>
       </StyledContainer>
-    );
-  }
+      <StyledFeedContainer>
+        {post ? (
+          <>
+            <Tweet post={post} />
+            <StyledContainer
+              borderBottom={"1px solid #ebeef0"}
+              padding={"16px"}
+            >
+              <TweetBox parentId={postId} />
+            </StyledContainer>
+
+            <StyledContainer minHeight={"53.5vh"}>
+              <CommentFeed postId={postId} />
+            </StyledContainer>
+          </>
+        ) : (
+          <StyledContainer justifyContent={"center"} alignItems={"center"}>
+            <Loader />
+          </StyledContainer>
+        )}
+      </StyledFeedContainer>
+    </StyledContainer>
+  )
 }
 
-export default PostPage;
+export default PostPage
